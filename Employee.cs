9467936950e998d3633d8102e3408f5a8879a648
ishan84 <ishan.swarnasinghe@gmail.com>
2020-02-26 21:48:34 -0700

    This is the fist file to this repo

---
+++ Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetMvcRestPoc.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+        public decimal Salary { get; set; }
+        public int[] TestArray { get; set; }
+        public TestObj[] TestObjs { get; set; }
+    }
+}
